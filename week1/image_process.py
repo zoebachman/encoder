dc497b83400d2771275cc330fb1fb72780c729ac
Zoe Bachman <zoe.bachman@gmail.com>
2016-09-14 02:25:44 -0400

    added w strings, not sure if number counts are right

--- week1/image_process.py
+++ week1/image_process.py
@@ -1,43 +1,50 @@
-#cleaned up code
-#printing amounts of white and black pixels
+#adding W option
 
 from PIL import Image
 im = Image.open("plain_heart.jpg")
-
-
 px = list(im.getdata())
-
-# white_pixels = []
-# black_pixels = []
 
 all_pixels = []
 
-count = 0
+b_count = 0 #1 or 0? maybe just immediately add 1 to count as soon as it sees letter
+w_count = 0 #not sure if I need separate counts, but just in case
 
 count_list = []
 
-
+#turn this into a function
 for i in px:
 	if i == (0,0,0):
-		all_pixels.append("W")
-	elif i == (255, 255, 255):
 		all_pixels.append("B")
+	elif i == (255, 255, 255):
+		all_pixels.append("W")
 
 # len(all_pixels) = 14470
 print(len(all_pixels))
 
-for z in range(len(all_pixels)): #getting position
+for z in range(len(all_pixels)): #getting position // maybe add one to end? doesn't it stop early?
   if all_pixels[z] == "B":
+  	b_count = b_count + 1 #add 1 to count to start...maybe adding too much? maybe this is how we isolate the 1s...
   	#check to see if next one is also B
   	if all_pixels[(z+1)] == "B":
   		#print("2B") 
-  		count = count + 1 #increment count by 1
+  		b_count = b_count + 1 #increment count by 1
   	elif all_pixels[(z+1)] == "W": #but I think the [z:z+1] will be helpful somewhere
   		# count_list.append(str(count) + "B")
-  		print(str(count) + "B")
-  		count = 0 #reset count to 0
-  	else:
-  		print ("no")
+  		print(str(b_count) + "B")
+  		b_count = 0 #reset count to 0
+  	# else:
+  	# 	print ("B")
+
+  elif all_pixels[z] == "W":
+  	w_count = w_count + 1 # add 1 to count to start
+  	#check to see if next one is also W
+  	if all_pixels[(z+1)] == "W":
+  		w_count = w_count + 1 #increment count by 1
+  	elif all_pixels[(z+1)] == "B":
+  		print(str(w_count) + "W")
+  		w_count = 0 #reset count to 0
+  	# else:
+  	# 	print("W")
     #print (z)
     #print(z+1) #position one over
     #print(all_pixels[z:(z+1)])
@@ -47,3 +54,6 @@ for z in range(len(all_pixels)): #getting position
 # 	print (i) #printing position in list
 
 
+#next steps: put them into one long list and convert back into a string
+# try and get them as a tuple: print count and letter separately
+#BONUS:minimize characters by only having a letter that's isolated not have a number next to it 
